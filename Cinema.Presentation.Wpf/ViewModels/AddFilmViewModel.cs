5164bc38d4f01ecae75e02bcb75f3215af63b523
melnyk-a <melnyk.an.il@gmail.com>
2018-09-22 20:40:18 +0300

    Refactor Cinema.Presentation.Wpf.ViewModels.AddFilmViewModel

--- Cinema.Presentation.Wpf/ViewModels/AddFilmViewModel.cs
+++ Cinema.Presentation.Wpf/ViewModels/AddFilmViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cinema.Presentation.Wpf.ViewModels
+﻿namespace Cinema.Presentation.Wpf.ViewModels
 {
     internal sealed class AddFilmViewModel
     {
