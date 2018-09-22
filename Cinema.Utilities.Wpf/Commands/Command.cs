1c6ba5195ec866a30ca0454419a97e9a06487e8f
melnyk-a <melnyk.an.il@gmail.com>
2018-09-22 20:56:10 +0300

    Add base classes for realization of Command pathern in WPF

---
+++ Cinema.Utilities.Wpf/Commands/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Cinema.Utilities.Wpf.Commands
+{
+    public abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public virtual bool CanExecute() => true;
+
+        public abstract void Execute();
+
+        protected virtual void OnCanExecuteChanged(EventArgs e)
+        {
+            CanExecuteChanged?.Invoke(this, e);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute();
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            Execute();
+        }
+    }
+}
