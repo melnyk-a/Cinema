5f8fe0aa8a84803f8de1107c41c3c00dd2e70645
melnyk-a <melnyk.an.il@gmail.com>
2018-09-22 20:39:42 +0300

    Add ViewModel with autnotify on property changed

---
+++ Cinema.Utilities.Wpf/ViewModels/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Cinema.Utilities.Wpf.ViewModels
+{
+    internal abstract class ViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+
+        protected void SetProperty<T>(ref T oldValue, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if(!oldValue?.Equals(newValue)??newValue!=null)
+            {
+                oldValue = newValue;
+
+                OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
