6520d11fa97e2214b71e0d10482c7d8622b9f820
melnyk-a <melnyk.an.il@gmail.com>
2018-09-22 15:29:26 +0300

    Add Film model

---
+++ Cinema.Domain.Models/Film.cs
@@ -0,0 +1,12 @@
+﻿namespace Cinema.Domain.Models
+{
+    internal sealed class Film
+    {
+        private readonly string name;
+
+        public Film(string name)
+        {
+            this.name = name;
+        }
+    }
+}
