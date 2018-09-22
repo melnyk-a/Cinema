99f883cfef43d6df5906d8ac71832ac8c891139e
melnyk-a <melnyk.an.il@gmail.com>
2018-09-22 18:55:09 +0300

    Add AddFilm() to Cinema.Data.Stub.StubCinemaDataGateway

--- Cinema.Data.Stub/StubCinemaDataGateway.cs
+++ Cinema.Data.Stub/StubCinemaDataGateway.cs
@@ -8,6 +8,12 @@ namespace Cinema.Data.Stub
     internal sealed class StubCinemaDataGateway : ICinemaDataGateway
     {
         private readonly ICollection<Film> films = new List<Film>();
+
+        public void AddFilm(Film film)
+        {
+            films.Add(film);
+        }
+
         public IEnumerable<Film> GetAllFilms()
         {
             films.Add(new Film("The House with a Clock in Its Walls",
