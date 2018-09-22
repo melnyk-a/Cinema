b938c336a18a7feb702b040892db7d4a824dd2b2
melnyk-a <melnyk.an.il@gmail.com>
2018-09-22 16:12:30 +0300

    Implement Cinema.Data.Stub by adding StubCinemaDataGateway and StubCinemaDataService

---
+++ Cinema.Data.Stub/StubCinemaDataGateway.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Cinema.Domain.Models;
+
+namespace Cinema.Data.Stub
+{
+    internal sealed class StubCinemaDataGateway : ICinemaDataGateway
+    {
+        private readonly ICollection<Film> films = new List<Film>();
+        public IEnumerable<Film> GetAllFilms()
+        {
+            films.Add(new Film("The House with a Clock in Its Walls"));
+            films.Add(new Film("Fahrenheit 11/9"));
+            films.Add(new Film("Life Itself"));
+
+            return films;
+        }
+    }
+}
---
+++ Cinema.Data.Stub/StubCinemaDataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Cinema.Data.Stub
+{
+    internal sealed class StubCinemaDataService : ICinemaDataService
+    {
+        public void CloseDataGateway(ICinemaDataGateway dataGateway)
+        {
+            // Do nothing.
+        }
+
+        public ICinemaDataGateway OpenDataGateway()
+        {
+            return new StubCinemaDataGateway();
+        }
+    }
+}
