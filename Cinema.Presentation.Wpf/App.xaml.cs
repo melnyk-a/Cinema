bfb866e58c3df54b6d83260ea38ff47570b17454
melnyk-a <melnyk.an.il@gmail.com>
2018-09-22 15:16:57 +0300

    Prepare presentation layer
    Add folders for realization of MVVVM pathern. Add MainWindowView. Prepare presentation layer for dispaly of MainWindowView.

--- Cinema.Presentation.Wpf/App.xaml.cs
+++ Cinema.Presentation.Wpf/App.xaml.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Cinema.Presentation.Wpf.Views;
 using System.Windows;
 
 namespace Cinema.Presentation.Wpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    internal partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var view = new MainView();
+            view.Show();
+        }
     }
 }
---
+++ Cinema.Presentation.Wpf/Views/MainWindowView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Cinema.Presentation.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    internal partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
