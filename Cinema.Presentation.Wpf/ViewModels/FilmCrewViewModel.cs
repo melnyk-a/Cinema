80ab349d0f4acc6553b708126cb5f89ed9b78620
melnyk-a <melnyk.an.il@gmail.com>
2018-09-23 17:17:34 +0300

    Implement Cinema.Presentation.Wpf.ViewModels.FilmCrewViewModel

--- Cinema.Presentation.Wpf/ViewModels/FilmCrewViewModel.cs
+++ Cinema.Presentation.Wpf/ViewModels/FilmCrewViewModel.cs
@@ -1,7 +1,24 @@
-﻿namespace Cinema.Presentation.Wpf.ViewModels
-{
-    internal sealed class FilmCrewViewModel
-    {
+﻿using Cinema.Utilities.Wpf.ViewModels;
 
+namespace Cinema.Presentation.Wpf.ViewModels
+{
+    public sealed class FilmCrewViewModel : ViewModel
+    {
+        private readonly string name;
+        private readonly string surname;
+        private readonly string position;
+
+        public FilmCrewViewModel(string name, string surname, string position)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.position = position;
+        }
+
+        public string Name => name;
+
+        public string Surname => surname;
+
+        public string Position => position;
     }
 }
