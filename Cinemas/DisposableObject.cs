0e94bca4f008e521b768f87a825a0e6feaf0cfe5
melnyk-a <melnyk.an.il@gmail.com>
2018-09-23 22:53:44 +0300

    Add DisposableObject conception to Utilities

---
+++ Cinemas/DisposableObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cinemas
+{
+    public abstract class DisposableObject : IDisposable
+    {
+        private bool isDisposed;
+
+        ~DisposableObject()
+        {
+            TryDispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            TryDispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected abstract void Dispose(bool disposing);
+
+        private void TryDispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                Dispose(disposing);
+
+                isDisposed = true;
+            }
+        }
+    }
+}
