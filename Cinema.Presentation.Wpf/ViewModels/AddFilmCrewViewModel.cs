2862e355bd4d9026fe74dcf7631a4bea9d499043
melnyk-a <melnyk.an.il@gmail.com>
2018-09-23 19:29:36 +0300

    Implement usage of DependsUponAttribute and RaiseCanExecuteDependsUponAttribute

--- Cinema.Presentation.Wpf/ViewModels/AddFilmCrewViewModel.cs
+++ Cinema.Presentation.Wpf/ViewModels/AddFilmCrewViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Cinema.Presentation.Wpf.ViewModels.Factories;
+using Cinema.Utilities.Wpf.Attributes;
 using Cinema.Utilities.Wpf.Commands;
 using Cinema.Utilities.Wpf.ViewModels;
 using System.Collections.Generic;
@@ -14,21 +15,23 @@ namespace Cinema.Presentation.Wpf.ViewModels
         private readonly ICollection<FilmCrewViewModel> crews = new ObservableCollection<FilmCrewViewModel>();
         private readonly IViewModelFactory viewModelFactory;
 
-        private string actorName;
-        private string actorSurname;
-        private string directorName;
-        private string directorSurname;
+        private string actorName = string.Empty;
+        private string actorSurname = string.Empty;
+        private string directorName = string.Empty;
+        private string directorSurname = string.Empty;
 
         public AddFilmCrewViewModel(IViewModelFactory viewModelFactory)
         {
             this.viewModelFactory = viewModelFactory;
 
-            addActorCommand = new DelegateCommand(() => crews.Add(viewModelFactory.CreateFilmCrewViewModel(actorName, actorSurname, "Actor")));
-            addDirectorCommand = new DelegateCommand(() => crews.Add(viewModelFactory.CreateFilmCrewViewModel(directorName, directorSurname, "Director")));
+            addActorCommand = new DelegateCommand(() => crews.Add(viewModelFactory.CreateFilmCrewViewModel(actorName, actorSurname, "Actor")), () => CanAddActor);
+            addDirectorCommand = new DelegateCommand(() => crews.Add(viewModelFactory.CreateFilmCrewViewModel(directorName, directorSurname, "Director")), () => CanAddDirector);
         }
 
+        [RaiseCanExecuteDependsUpon(nameof(CanAddActor))]
         public ICommand AddActorCommand => addActorCommand;
 
+        [RaiseCanExecuteDependsUpon(nameof(CanAddDirector))]
         public ICommand AddDirectorCommand => addDirectorCommand;
 
         public string ActorName
@@ -42,6 +45,16 @@ namespace Cinema.Presentation.Wpf.ViewModels
             get => actorSurname;
             set => SetProperty(ref actorSurname, value);
         }
+
+        [DependsUpon(nameof(ActorName))]
+        [DependsUpon(nameof(ActorSurname))]
+        public bool CanAddActor => ActorName.Length > 0 && ActorSurname.Length > 0;
+
+        [DependsUpon(nameof(DirectorName))]
+        [DependsUpon(nameof(DirectorSurname))]
+        public bool CanAddDirector => DirectorName.Length > 0 && DirectorSurname.Length > 0;
+
+        public bool FilmCrewSetUp => true;
 
         public IEnumerable<FilmCrewViewModel> Crews => crews;
 
--- Cinema.Presentation.Wpf/ViewModels/AddFilmViewModel.cs
+++ Cinema.Presentation.Wpf/ViewModels/AddFilmViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Cinema.Domain.Models;
 using Cinema.Presentation.Wpf.ViewModels.Factories;
+using Cinema.Utilities.Wpf.Attributes;
 using Cinema.Utilities.Wpf.Commands;
 using System;
 using System.Windows.Input;
@@ -9,19 +10,28 @@ namespace Cinema.Presentation.Wpf.ViewModels
     public sealed class AddFilmViewModel : SwitchContentViewModel
     {
         private readonly AddFilmCrewViewModel addFilmCrewViewModel;
+        private readonly ICommand addFilmCommand;
         private readonly ICommand cancelCommand;
 
-        private Language selectedLanguage;
-        private DateTime selectedDate = DateTime.Now;
-        private string title;
+        private Language selectedLanguage = Language.Unspecified;
+        private DateTime selectedDate = DateTime.MinValue;
+        private string title = string.Empty;
 
         public AddFilmViewModel(IViewModelFactory viewModelFactory)
         {
+            addFilmCommand = new DelegateCommand(AddFilm, () => CanAddFilm);
             addFilmCrewViewModel = viewModelFactory.CreateAddFilmCrewViewModel();
             cancelCommand = new DelegateCommand(() => ViewModelManager.SetFilmListViewModel());
         }
 
+        [RaiseCanExecuteDependsUpon(nameof(CanAddFilm))]
+        public ICommand AddFilmCommand => addFilmCommand;
         public object AddFilmCrewViewModel => addFilmCrewViewModel;
+
+        [DependsUpon(nameof(Title))]
+        [DependsUpon(nameof(SelectedDate))]
+        [DependsUpon(nameof(SelectedLanguage))]
+        public bool CanAddFilm => Title.Length > 0 && SelectedDate != DateTime.MinValue && SelectedLanguage != Language.Unspecified && addFilmCrewViewModel.FilmCrewSetUp;
 
         public ICommand CancelCommand
         {
@@ -44,6 +54,11 @@ namespace Cinema.Presentation.Wpf.ViewModels
         {
             get => title;
             set => SetProperty(ref title, value);
+        }
+
+        public void AddFilm()
+        {
+
         }
     }
 }
