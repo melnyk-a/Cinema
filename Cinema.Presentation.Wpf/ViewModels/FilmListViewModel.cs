dce1b9460486964941e24832c4f4df5b7b90832a
melnyk-a <melnyk.an.il@gmail.com>
2018-09-23 12:56:51 +0300

    Add AddFilmCommand realization to Cinema.Presentation.Wpf.ViewModels.FilmListViewModel

--- Cinema.Presentation.Wpf/ViewModels/FilmListViewModel.cs
+++ Cinema.Presentation.Wpf/ViewModels/FilmListViewModel.cs
@@ -1,25 +1,35 @@
 ﻿using Cinema.Domain;
 using Cinema.Domain.Models;
 using Cinema.Presentation.Wpf.ViewModels.Factories;
-using Cinema.Utilities.Wpf.ViewModels;
+using Cinema.Utilities.Wpf.Commands;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Cinema.Presentation.Wpf.ViewModels
 {
-    public sealed class FilmListViewModel : ViewModel
+    public sealed class FilmListViewModel : SwitchContentViewModel
     {
+        private ICommand addFilmCommand;
         private readonly ICollection<FilmViewModel> films = new ObservableCollection<FilmViewModel>();
 
         public FilmListViewModel(ICinemaProvider cinemaProvider, IViewModelFactory viewModelFactory)
         {
-            foreach (Film film in cinemaProvider.GetAllFilms())
-            {
-                var viewModel = viewModelFactory.CreateFilmViewModel(film);
-                films.Add(viewModel);
-            }
+            addFilmCommand = new DelegateCommand(() => ViewModelManager.SetAddFilmViewModel());
+            foreach (Film film in cinemaProvider.GetAllFilms()) foreach (Film film in cinemaProvider.GetAllFilms())
+                {
+                    {
+                        var viewModel = viewModelFactory.CreateFilmViewModel(film); var viewModel = viewModelFactory.CreateFilmViewModel(film);
+                        films.Add(viewModel); films.Add(viewModel);
+                    }
+                }
         }
 
         public IEnumerable<FilmViewModel> Films => films;
+
+        public ICommand AddFilmCommand
+        {
+            get => addFilmCommand;
+        }
     }
 }
