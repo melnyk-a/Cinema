b4957713e62709b72d938eb36e2525b87d468391
melnyk-a <melnyk.an.il@gmail.com>
2018-09-23 12:31:52 +0300

    Add conception of elements who can change content of parent window

---
+++ Cinema.Presentation.Wpf/ViewModels/SwitchContentViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Cinema.Utilities.Wpf.ViewModels;
+
+namespace Cinema.Presentation.Wpf.ViewModels
+{
+    public abstract class SwitchContentViewModel : ViewModel
+    {
+        public ViewModelManager ViewModelManager { get; set; }
+    }
+}
