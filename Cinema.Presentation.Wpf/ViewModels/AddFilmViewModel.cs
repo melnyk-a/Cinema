eea8791811948cfcf4b6d968dad17368c383efb8
melnyk-a <melnyk.an.il@gmail.com>
2018-09-23 17:02:51 +0300

    Bind AddFilmViewModel with AddFilmView

--- Cinema.Presentation.Wpf/ViewModels/AddFilmViewModel.cs
+++ Cinema.Presentation.Wpf/ViewModels/AddFilmViewModel.cs
@@ -1,21 +1,49 @@
-﻿using Cinema.Utilities.Wpf.Commands;
-using Cinema.Utilities.Wpf.ViewModels;
+﻿using Cinema.Domain.Models;
+using Cinema.Presentation.Wpf.ViewModels.Factories;
+using Cinema.Utilities.Wpf.Commands;
+using System;
 using System.Windows.Input;
 
 namespace Cinema.Presentation.Wpf.ViewModels
 {
     public sealed class AddFilmViewModel : SwitchContentViewModel
     {
-        private ICommand cancelCommand;
+        private readonly AddFilmCrewViewModel addFilmCrewViewModel;
+        private readonly ICommand cancelCommand;
 
-        public AddFilmViewModel()
+        private Language selectedLanguage;
+        private DateTime selectedDate = DateTime.Now;
+        private string title;
+
+        public AddFilmViewModel(IViewModelFactory viewModelFactory)
         {
+            addFilmCrewViewModel = viewModelFactory.CreateAddFilmCrewViewModel();
             cancelCommand = new DelegateCommand(() => ViewModelManager.SetFilmListViewModel());
         }
+
+        public object AddFilmCrewViewModel => addFilmCrewViewModel;
 
         public ICommand CancelCommand
         {
             get => cancelCommand;
+        }
+
+        public Language SelectedLanguage
+        {
+            get => selectedLanguage;
+            set => SetProperty(ref selectedLanguage, value);
+        }
+
+        public DateTime SelectedDate
+        {
+            get => selectedDate;
+            set => SetProperty(ref selectedDate, value);
+        }
+
+        public string Title
+        {
+            get => title;
+            set => SetProperty(ref title, value);
         }
     }
 }
