1047cb6f7e3d2beeb420a88f2322d35a302cc194
melnyk-a <melnyk.an.il@gmail.com>
2018-09-23 13:08:18 +0300

    Add cancelCommand in AddFilmViewModel and bind AddFilm to it

--- Cinema.Presentation.Wpf/ViewModels/AddFilmViewModel.cs
+++ Cinema.Presentation.Wpf/ViewModels/AddFilmViewModel.cs
@@ -1,9 +1,21 @@
-﻿using Cinema.Utilities.Wpf.ViewModels;
+﻿using Cinema.Utilities.Wpf.Commands;
+using Cinema.Utilities.Wpf.ViewModels;
+using System.Windows.Input;
 
 namespace Cinema.Presentation.Wpf.ViewModels
 {
     public sealed class AddFilmViewModel : SwitchContentViewModel
     {
+        private ICommand cancelCommand;
 
+        public AddFilmViewModel()
+        {
+            cancelCommand = new DelegateCommand(() => ViewModelManager.SetFilmListViewModel());
+        }
+
+        public ICommand CancelCommand
+        {
+            get => cancelCommand;
+        }
     }
 }
