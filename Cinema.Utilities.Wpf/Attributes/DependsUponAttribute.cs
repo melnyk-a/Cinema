57b3b067c6d779e376944fee535e80e4dd81ce79
melnyk-a <melnyk.an.il@gmail.com>
2018-09-23 19:04:49 +0300

    Add attributes for autocall CanExecuteCommand

---
+++ Cinema.Utilities.Wpf/Attributes/DependsUponAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cinema.Utilities.Wpf.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public sealed class DependsUponAttribute : Attribute
+    {
+        private readonly string propertyName;
+
+        public DependsUponAttribute(string propertyName)
+        {
+            this.propertyName = propertyName;
+        }
+
+        public string PropertyName => propertyName;
+    }
+}
