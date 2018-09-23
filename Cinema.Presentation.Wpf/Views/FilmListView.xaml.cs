1335d0f56ea6efc1baa49a6284d29d0d6c360aea
melnyk-a <melnyk.an.il@gmail.com>
2018-09-23 22:08:00 +0300

    Refactor Cinema.Presentation.Wpf.Views

--- Cinema.Presentation.Wpf/Views/FilmListView.xaml.cs
+++ Cinema.Presentation.Wpf/Views/FilmListView.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace Cinema.Presentation.Wpf.Views
 {
-    /// <summary>
-    /// Interaction logic for FilmListView.xaml
-    /// </summary>
     public partial class FilmListView : UserControl
     {
         public FilmListView()
