f23e5f4c2f3c54c3c6b6529d682e60676c6b2875
melnyk-a <melnyk.an.il@gmail.com>
2018-09-27 11:15:21 +0300

    Add FilmDto in Cinema.Data.SqlServerOdbc

---
+++ Cinema.Data.SqlServerOdbc/Dtos/FilmDto.cs
@@ -0,0 +1,16 @@
+﻿using Cinema.Domain.Models;
+using System;
+
+namespace Cinema.Data.SqlServerOdbc.Dtos
+{
+    internal sealed class FilmDto
+    {
+        public int Id { get; set; }
+
+        public Language Language { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Title { get; set; }
+    }
+}
