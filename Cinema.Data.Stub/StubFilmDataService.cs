92415d783f12e2446e4009f1b7dba996f484d08f
melnyk-a <melnyk.an.il@gmail.com>
2018-09-28 23:05:30 +0300

    close #5

--- Cinema.Data.Stub/StubFilmDataService.cs
+++ Cinema.Data.Stub/StubFilmDataService.cs
@@ -2,11 +2,6 @@
 {
     public sealed class StubFilmDataService : IFilmDataService
     {
-        public void CloseDataGateway(IFilmDataGateway dataGateway)
-        {
-            // Do nothing.
-        }
-
         public IFilmDataGateway OpenDataGateway()
         {
             return new StubFilmDataGateway();
