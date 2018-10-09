0a6f13c4e36bb1f40f6915861d48e81eb286a801
melnyk-a <melnyk.an.il@gmail.com>
2018-10-09 19:13:18 +0300

    Add property Films to FilmDbContext

--- Cinema.Data.EntityFramework/FilmDbContext.cs
+++ Cinema.Data.EntityFramework/FilmDbContext.cs
@@ -1,9 +1,10 @@
-﻿using System.Data.Entity;
+﻿using Cinema.Utilities.Data.Dtos;
+using System.Data.Entity;
 
 namespace Cinema.Data.EntityFramework
 {
     internal sealed class FilmDbContext : DbContext
     {
-
+        DbSet<FilmDto> Films { get; set; }
     }
 }
